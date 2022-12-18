6d5b3a16a348705ee1170767c9a020085b4cd23a
LIZA <vlizalizaliza@mail.ru>
2022-12-18 21:05:33 +0300

    Добавьте файлы проекта.

---
+++ лаба8з1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include<stdlib.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n, m;
+    printf("Введите значения n и m через пробел:");
+    scanf_s("%d %d", &n, &m);
+    for (int i = m, s = 0; i >= n; i--, s += i) printf("выполнено i=%d раз\n", i + 1), printf("результат s=%d\n", s);
+
+}
